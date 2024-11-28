016cb669f8595f19c335bf87301c54adb1169e56
Ahmad Hedayatpour <ahmadhedayatpour2002@gmail.com>
2024-11-28 20:42:32 +0330

    change header style

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,10 +4,12 @@ import UserCenter from "@/components/layout/header/userCenter";
 
 export default function Header() {
   return (
-    <header className="container flex gap-4 pt-6 pb-4 items-center justify-between">
-      <Logo />
-      <SearchBox />
-      <UserCenter />
+    <header className="border-b border-l-light-silver shadow">
+      <div className="container flex gap-4 pt-6 pb-4 items-center justify-between">
+        <Logo />
+        <SearchBox />
+        <UserCenter />
+      </div>
     </header>
   );
 }
