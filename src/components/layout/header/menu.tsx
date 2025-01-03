f835197eaed22019dece8f285f361d106e5dd399
Ahmad Hedayatpour <ahmadhedayatpour2002@gmail.com>
2025-01-03 16:51:05 +0330

    FIX: Menu responsive

--- src/components/layout/header/menu.tsx
+++ src/components/layout/header/menu.tsx
@@ -14,12 +14,14 @@ export default function Menu() {
   return (
     <>
       <nav className="absolute text-white/60 text-xs sm:text-sm w-full right-0 top-full z-50 px-2 pt-4 lg:pt-3">
-        <ul className="flex md:gap-8 justify-between md:justify-start bg-dark-purple py-5 px-2 sm:px-4 md:px-9 rounded-xl">
+        <ul className="flex md:gap-8 justify-between md:justify-start bg-dark-purple py-5 px-4 md:px-9 rounded-xl">
           {menuItems.map((item, index) => (
             <li key={index}>
               <Link
                 href={item.address}
-                className={`${isActive === item.address && "text-white"}`}
+                className={`${isActive === item.address && "text-white"} ${
+                  index === menuItems.length - 1 && "hidden sm:block"
+                }`}
               >
                 {item.title}
               </Link>
