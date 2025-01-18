fb9c187b76f5bf2a6c36c4fdcaf7cef0f6605227
Ahmad Hedayatpour <ahmadhedayatpour2002@gmail.com>
2025-01-19 02:46:42 +0330

    Add CartDetails

--- src/components/cart/cart.tsx
+++ src/components/cart/cart.tsx
@@ -4,6 +4,7 @@ import { useStore } from "@/stores/useStore";
 import BookCard from "@/components/bookCard";
 import CartTitle from "@/components/cart/cartTitle";
 import EpmtyCart from "@/components/cart/epmtyCart";
+import CartDetails from "./cartDetails";
 
 export default function Cart() {
   const { getCartItems } = useStore();
@@ -14,11 +15,16 @@ export default function Cart() {
       <CartTitle />
 
       {cartIds.length ? (
-        <div className="w-fit mx-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-y-6 gap-x-4 justify-center pb-9">
-          {cartIds.map((id) => (
-            <BookCard key={id} />
-          ))}
-        </div>
+        <>
+          <div className="w-fit mx-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-y-6 gap-x-4 justify-center pb-9">
+            {cartIds.map((id) => (
+              <BookCard key={id} />
+            ))}
+          </div>
+          <div className="h-[572px]">
+            <CartDetails />
+          </div>
+        </>
       ) : (
         <EpmtyCart />
       )}
